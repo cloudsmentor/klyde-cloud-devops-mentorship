99ef0f321d130cfff9158eb261020ffd13354a06
Victor Garcia <victorgrubiodl@gmail.com>
2024-03-04 18:44:20 +0100

    feat: working sqlite

--- py/app.py
+++ py/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
 
+# Load the environment variables from the .env file
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "FLASK_DATABASE_URL", "sqlite:///test.db"
+)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
