e7d2fd8cacf78dd1595addb499ded10d3dfb1f3a
Klyde <klyde.moradeyo.dev@gmail.com>
2024-03-06 22:22:41 +0000

    Fix views

--- py/views.py
+++ py/views.py
@@ -1,5 +1,6 @@
 from models.user import User
 from flask import request
+from flask import jsonify
 
 def init_app_routes(app, db):
     @app.route("/ping", methods=["GET"])
@@ -12,18 +13,30 @@ def init_app_routes(app, db):
         new_user = User(name=data["name"])
         db.session.add(new_user)
         db.session.commit()
+        app.logger.info(f"Created User'{new_user.id}:{data['name']}'")
         return {"id": new_user.id}
 
-    @app.route("/users", methods=["GET"])
+    @app.route("/get_users", methods=["GET"])
     def get_users():
         all_users = User.query.all()
-        return {"users": all_users}
+        users_list = []
+        for user in all_users:
+            user_dict = {
+                'id': user.id,
+                'name': user.name
+            }
+            users_list.append(user_dict)
+        
+        app.logger.info(f"Retrieved all users!")
+        return {"users": users_list}
 
     @app.route("/users/<id>", methods=["GET"])
     def get_user(id):
         user = User.query.get(id)
         if user is None:
+            app.logger.error(f"User {id} NOT FOUND")
             return {"error": "not found"}, 404
+        app.logger.info(f"Retrieved User {id}")
         return {"data": user}
 
     @app.route("/users/<id>", methods=["PUT"])
@@ -31,16 +44,20 @@ def init_app_routes(app, db):
         data = request.get_json()
         user = User.query.get(id)
         if user is None:
+            app.logger.error(f"User {id} NOT FOUND")
             return {"error": "not found"}, 404
         user.name = data["name"]
         db.session.commit()
+        app.logger.info(f"Updated User '{id}:{data['name']}'")
         return {"success": True}
 
     @app.route("/users/<id>", methods=["DELETE"])
     def delete_user(id):
         user = User.query.get(id)
         if user is None:
+            app.logger.error(f"User {id} NOT FOUND")
             return {"error": "not found"}, 404
         db.session.delete(user)
         db.session.commit()
+        app.logger.info(f"Deleted User {id}")
         return {"success": True}
