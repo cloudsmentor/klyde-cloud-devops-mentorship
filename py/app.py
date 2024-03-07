e147526e197340f785a61f5133905ed9721f98b9
Klyde Moradeyo <50842133+Klyde-Moradeyo@users.noreply.github.com>
2024-03-07 11:41:56 +0000

    Fix flask api implementation (#13)
    Co-authored-by: Klyde <klyde.moradeyo.dev@gmail.com>
    Co-authored-by: devsmentor <156417429+devsmentor@users.noreply.github.com>

--- py/app.py
+++ py/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from views import init_app_routes
+from extensions import db, migrate
 import os
-
 
 # Load the environment variables from the .env file
 
@@ -10,11 +9,14 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
     "FLASK_DATABASE_URL", "sqlite:///test.db"
 )
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
 
 if __name__ == "__main__":
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    init_app_routes(app, db)
+    app.run(debug=True, host='0.0.0.0')
--- py/models/user.py
+++ py/models/user.py
@@ -1,5 +1,4 @@
-from app import db
-
+from extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
