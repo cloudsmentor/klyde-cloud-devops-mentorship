04849ee11ed0741dcd37e501a5aa4e9ed51dccba
Klyde <klyde.moradeyo.dev@gmail.com>
2024-03-13 20:44:42 +0000

    bump ecr module version

--- terraform/flask_api.tf
+++ terraform/flask_api.tf
@@ -3,7 +3,7 @@
 ########################
 module "ecr" {
   source  = "km-tf-registry.onrender.com/klyde-moradeyo__dev-generic-tf-modues/ecr/aws"
-  version = "0.0.3"
+  version = "0.0.4"
 
   name = var.name
   tags = module.tags.tags
