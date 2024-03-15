9c20c89d92b8200733ea754196b4b92b8efd05eb
Klyde <klyde.moradeyo.dev@gmail.com>
2024-03-15 18:08:29 +0000

    Update terrraform manifests

--- terraform/flask_api.tf
+++ terraform/flask_api.tf
@@ -2,8 +2,7 @@
 #         ECR          #
 ########################
 module "ecr" {
-  source  = "km-tf-registry.onrender.com/klyde-moradeyo__dev-generic-tf-modues/ecr/aws"
-  version = "0.0.3"
+  source  = "./modules/ecr"
 
   name = var.name
   tags = module.tags.tags
