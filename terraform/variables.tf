d50e56266efb566a0794ed56d7206fce9333044b
Klyde <klyde.moradeyo.dev@gmail.com>
2024-03-17 14:30:11 +0000

    Add tag related variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,27 @@ variable "name" {
   type        = string
   default     = "flask-api"
 }
+
+variable "project" {
+  description = "The name of the project"
+  type        = string
+  default     = "flask-api"
+}
+
+variable "client" {
+  description = "The client for the project"
+  type        = string
+  default     = "public"
+}
+
+variable "owner" {
+  description = "The owner of the project"
+  type        = string
+  default     = "Klyde-Moradeyo"
+}
+
+variable "environment" {
+  description = "The project environment"
+  type        = string
+  default     = "dev"
+}
