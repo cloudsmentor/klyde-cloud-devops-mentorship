51783f363c87df4ebf3e782e2046571751b7712b
Klyde <klyde.moradeyo.dev@gmail.com>
2024-03-19 13:28:55 +0000

    Update modules to stop using "this" var name

--- terraform/modules/iam-role/outputs.tf
+++ terraform/modules/iam-role/outputs.tf
@@ -1,9 +1,9 @@
 output "iam_role_arn" {
   description = "The ARN of the IAM role."
-  value       = aws_iam_role.this.arn
+  value       = aws_iam_role.iam_role.arn
 }
 
 output "iam_role_name" {
   description = "The name of the IAM role."
-  value       = aws_iam_role.this.name
+  value       = aws_iam_role.iam_role.name
 }
