7f2beb4f002c7d8af77b50197bd6c2052326d092
Klyde <klyde.moradeyo.dev@gmail.com>
2024-03-24 01:56:45 +0000

    Fix aws-ebs-csi-driver add-on

--- terraform/eks-addons.tf
+++ terraform/eks-addons.tf
@@ -1,7 +1,16 @@
-locals {
+locals {  
     ebs_csi_driver_name = "aws-ebs-csi-driver"
     ebs_csi_driver_version = "v1.28.0-eksbuild.1"
+    oidc_issuer_str = replace(module.eks_cluster.eks_oidc_issuer_url, "https://", "")
 }
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  url             = module.eks_cluster.eks_oidc_issuer_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+  tags            = module.tags.tags
+}
+
 
 ############################
 #        IAM Role          #
@@ -22,13 +31,13 @@ module "eks_ebs_csi_iam_role" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${module.eks_cluster.eks_oidc_issuer_url}"
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_issuer_str}"
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "${module.eks_cluster.eks_oidc_issuer_url}:aud" = "sts.amazonaws.com"
-            "${module.eks_cluster.eks_oidc_issuer_url}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+            "${local.oidc_issuer_str}:aud" = "sts.amazonaws.com"
+            "${local.oidc_issuer_str}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
           }
         }
       }
